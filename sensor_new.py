5f2257409374ccd9d3d110e4e05cfb104963c232
MezdaOgu <oguzmezda@gmail.com>
2025-11-27 15:05:59 +0100

    Oguz: Sensoren 2.1.1

--- sensor_new.py
+++ sensor_new.py
@@ -3,11 +3,19 @@
 
 import time
 import board
+from board import *
 import adafruit_ccs811
+import busio
 
 ccs811_i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 ccs811 = adafruit_ccs811.CCS811(ccs811_i2c)
+
+i2c = busio.I2C(SCL, SDA)
+i2c.try_lock()
+print(i2c.scan())
+i2c.unlock()
+i2c.deinit()
 
 # Wait for the sensor to be ready
 while not ccs811.data_ready:
